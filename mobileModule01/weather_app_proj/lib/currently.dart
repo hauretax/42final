7bcfeef8a16c6ce1d52c8428320cc5bf187facbb
Huggo <31890474+hauretax@users.noreply.github.com>
2024-06-19 17:59:44 +0200

    see you

---
+++ mobileModule01/weather_app_proj/lib/currently.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CurrentlyTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Currently'));
+  }
+}
